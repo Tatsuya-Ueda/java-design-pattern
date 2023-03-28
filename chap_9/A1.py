6f90a01b20218876caf66da4341e2080f84d2821
t_ueda <74095322+Tatsuya-Ueda@users.noreply.github.com>
2023-03-28 23:45:31 +0900

    Create A1.py

---
+++ chap_9/A1.py
@@ -0,0 +1,95 @@
+from abc import ABC, abstractmethod
+import random
+
+
+class Display:
+    def __init__(self, impl: "DisplayImpl") -> None:
+        self.__impl: "DisplayImpl" = impl
+
+    def open(self) -> None:
+        self.__impl.raw_open()
+
+    def print(self) -> None:
+        self.__impl.raw_print()
+
+    def close(self) -> None:
+        self.__impl.raw_close()
+
+    def display(self):
+        self.open()
+        self.print()
+        self.close()
+
+
+class RandomCountDisplay(Display):
+    def __init__(self, impl: "DisplayImpl") -> None:
+        super().__init__(impl)
+
+    def multi_display(self, times: int):
+        self.open()
+        for _ in range(random.randint(0, times)):
+            self.print()
+        self.close()
+
+
+class CountDisplay(Display):
+    def __init__(self, impl: "DisplayImpl") -> None:
+        super().__init__(impl)
+
+    def multi_display(self, times: int):
+        self.open()
+        for _ in range(times):
+            self.print()
+        self.close()
+
+
+class DisplayImpl(ABC):
+    @abstractmethod
+    def raw_open(self):
+        pass
+
+    @abstractmethod
+    def raw_print(self):
+        pass
+
+    @abstractmethod
+    def raw_close(self):
+        pass
+
+
+class StringDisplayImpl(DisplayImpl):
+    def __init__(self, string) -> None:
+        self.__string = string
+
+    # @override
+    def raw_open(self):
+        self.print_line()
+
+    # @override
+    def raw_print(self):
+        print("|{}|".format(self.__string))
+
+    # @override
+    def raw_close(self):
+        self.print_line()
+
+    def print_line(self):
+        print("+{}+".format("-" * len(self.__string)))
+
+
+class Main:
+    def __init__(self) -> None:  # Javaのmain関数を模して，これをmainとする
+        # d1: Display = Display(StringDisplayImpl("Hello, Japan."))
+        # d2: Display = CountDisplay(StringDisplayImpl("Hello, World."))
+        # d3: CountDisplay = CountDisplay(StringDisplayImpl("Hello, Universe."))
+
+        # d1.display()
+        # d2.display()
+        # d3.display()
+        # d3.multi_display(5)
+
+        d4 = RandomCountDisplay(StringDisplayImpl("Hello."))
+        d4.multi_display(10)
+
+
+Main()
