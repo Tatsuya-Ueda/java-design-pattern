81bd5397da6b930d936b89ac0c223505f15e230f
t_ueda <74095322+Tatsuya-Ueda@users.noreply.github.com>
2023-03-31 00:47:39 +0900

    Update enumtest.py

--- chap_10/enumtest.py
+++ chap_10/enumtest.py
@@ -1,6 +1,24 @@
 """
 enumモジュールに初めて触れる人のための実験コード
-https://atmarkit.itmedia.co.jp/ait/articles/2206/14/news020.html
+・アセットとコード中の名前の対応付けをするために使えそう．ゲームとかで．
+・
 """
 
 from enum import Enum
+
+
+class Role(Enum):
+    ROCK = 0
+    SCISSOR = 1
+    PAPER = 2
+
+
+print(Role.PAPER)
+print(Role(0))
+print(Role.PAPER.name)
+print(Role(0).name)  # value to member
+print(Role.PAPER.value)
+print(Role["PAPER"])  # キーでもOK
+# print(Role.PAPER < Role.SCISSOR)  # 順序は定義されていない．valueを取り出すのがよいかも
+
+[1, 2, 3][Role.PAPER]
